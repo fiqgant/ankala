c5e63e2b2e6df390e19c3bd7d93f176c712842cf
fiqgant <fiqgant@gmail.com>
2025-07-19 11:05:19 +0700

    first commit

---
+++ src/view-trip/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="my-7 text-center text-gray-500">
+      <h2 className="text-xl font-bold">Ankala Journey</h2>
+      <p className="text-sm">
+        Personalized, Authentic, and Sustainable Travel Plans
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
